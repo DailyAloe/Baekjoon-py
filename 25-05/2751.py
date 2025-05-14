0cc04fccbf34b1fc1fc220c23d77ad7bd1fd0e1f
Song SeChang <128244606+DailyAloe@users.noreply.github.com>
2025-05-14 22:31:10 +0900

    Update 2751.py

--- 25-05/2751.py
+++ 25-05/2751.py
@@ -8,3 +8,14 @@ for i in range(howmany):
     numList.append(num)
 numList.sort()
 print('\n'.join(map(str,numList)))
+
+#tm5323의 코드 변형
+import sys
+input = sys.stdin.readline
+
+a=[None]*2000001 #bool array를 -1,000,000~1,000,000까지 커버하도록 만듦
+for _ in range(int(input())):
+    i = int(input())
+    a[i+1000000]=1 #-1,000,000은 0번 인덱스에 위치하도록
+print("\n".join(str(i) for i in range(-1000000,1000001,1) if a[i+1000000]))
+#for -> if -> 명령(str)
